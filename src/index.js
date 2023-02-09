d6de1b3b03cb6acf26ad3eca74ee953c494fd1f0
Natallia-Lu <kali.laska@mail.ru>
2023-02-09 20:55:44 +0100

    feat: add reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+  let str = String(n)
+    let i = 0;
+  let result = ''
+
+if (str[0] === '-') {
+    i = 1
+} else  i = 0;
+
+  while(i < str.length) {
+    result = str[i] + result
+    i = i + 1
+}
+let res = Number.result
+return result
 }
